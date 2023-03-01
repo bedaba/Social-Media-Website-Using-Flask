184c82008d491ab0ce83ad25db9f7b3767805907
bedaba edward <bedaba15@gmail.com>
2023-03-01 15:07:06 +0200

    first commit

---
+++ flaskproject/models.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from itsdangerous.url_safe import URLSafeTimedSerializer as Serializer
+from flaskproject import db, login_manager
+from flask import current_app
+from flask_login import UserMixin  # Class which provides the basic parameters to validate user login 
+
+
+@login_manager.user_loader		# Required for flask-login extension to load user from user_id stored in session
+def load_user(user_id):
+	return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=300):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            return None
+        return User.query.get(user_id)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+    def get_posts(self):
+        return Post.query.filter_by(user_id=self.id).all()
+
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	title = db.Column(db.String(100), nullable = False)
+	date_posted = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+	content = db.Column(db.Text, nullable = False)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable = False) #Here 'user.id' represents 'id' 
+																				#column of table named 'user'
+																				#(Automatically converted to lower 
+																				#case by 'User' class)
+	def __repr__(self):
+		return f"User('{self.title}', '{self.date_posted}')"
+
